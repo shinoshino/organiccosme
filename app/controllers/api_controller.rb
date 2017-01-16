67b41b60155048fd4ec433d69959c48e2862c793
shinoshino <chiai.shino0824@gmail.com>
2017-01-16 20:28:11 +0900

    Revert "Revert "update design""
    This reverts commit 53885037c5ab55fc158ef509b39c70a5743d1782.

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,18 @@
+class ApiController < ApplicationController
+
+
+
+# require 'net/http'
+# require 'uri'
+# require 'rexml/document'
+
+# url = URI.parse(URI.escape("https://www.street-academy.com/api/v1/"))
+# res = Net::HTTP.start(url.host, url.port){|http|
+#     http.get(url.path + "?" + url.query);
+# }
+
+# doc = REXML::Document.new(res.body)
+
+# doc.elements.each('results/shop/') {|i|puts i.elements['name'].text}
+
+end
---
+++ app/models/event.rb
@@ -0,0 +1,18 @@
+    require 'net/http'
+    require 'uri'
+    require 'pry'
+    require 'json'
+
+class Event < ActiveRecord::Base
+  def self.create_events_from_api
+    url = URI.parse(URI.escape("https://www.street-academy.com/api/v1/events?category=19"))
+    res = Net::HTTP.start(url.host, url.port, use_ssl: true){|http|
+        http.get(url.path + "?" + url.query);
+    }
+    obj = JSON.parse(res.body)
+
+    obj['events'].each do | event |
+      Event.create(title: event['title'])
+    end
+  end
+end
